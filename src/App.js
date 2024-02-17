e794dd1d080bd3e539b670eac59b123f1f72a30a
GOKULA KANNAN V <99313546+gokulakannanv04@users.noreply.github.com>
2024-02-17 21:48:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
-
 import ChatbotForm from './pages/chatbotform';
 import Chat from './pages/chat';
 import Contact from './pages/contact';
 
+
 function App() {
-  return (
-    <div>
-      <Helmet>
-        {/* Google Analytics tag */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6XXKV8QRLX"></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-6XXKV8QRLX');
-          `}
-        </script>
-      </Helmet>
-
-      <Router>
-        <Routes>
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/" element={<ChatbotForm />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-}
-
-export default App;
+    return (
+        <div>
+          <Router>
+          <Routes>
+            <Route path="/chat" element={<Chat/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/" element={<ChatbotForm/>} />
+          </Routes>
+          </Router>
+        </div>
+      );
+    }
+    
+    export default App;
